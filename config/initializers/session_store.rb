006fca7e3351abd45d8ab4838e156241743754c1
James Brennan <james@carbonfive.com>
2013-06-15 21:46:02 -0700

    raygun-zapped skeleton

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JamesTheGiant::Application.config.session_store :cookie_store, key: '_james_the_giant_session'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+JamesTheGiant::Application.routes.draw do
+
+  root to: 'pages#root'
+
+end
