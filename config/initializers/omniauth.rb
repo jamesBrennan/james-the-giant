bb0514916abb0ccb1fb5c6ed090b40994e711910
James Brennan <james@carbonfive.com>
2014-05-27 10:34:50 -0700

    Change oath constants.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer if Rails.env.test? or Rails.env.development?
-  provider :google_oauth2, ENV['JTG_GOOGLE_CLIENT_ID'], ENV['JTG_GOOGLE_API_KEY'], {:prompt => "select_account"}
+  provider :google_oauth2, ENV['JTG_GOOGLE_CLIENT_ID'], ENV['JTG_GOOGLE_SECRET'], {:prompt => "select_account"}
 end
 
 # https://github.com/intridea/omniauth/wiki/FAQ#omniauthfailureendpoint-does-not-redirect-in-development-mode
